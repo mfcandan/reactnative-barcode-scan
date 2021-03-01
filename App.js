a5908e8506dc4cc6e329a6a6fae65c411fda9098
unknown <mfatihcandan@gmail.com>
2021-03-01 13:11:00 +0300

    Welcome screen

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Styles from './style'
-import { Text, View } from 'react-native';
+import styles from './style'
+import { TouchableOpacity, Text, View, } from 'react-native';
 
 export default function App() {
   return (
-    <View style={Styles.container}>
-      <Text>React Barcode Scan</Text>
-      <Text>SCAN ME!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.containerWelcome}>
+        <Text style={styles.buttonText}>Welcome to QR Signature</Text>
+      </View>
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.button}
+        ><Text style={styles.buttonText}>Scan QR Code</Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
--- style.js
+++ style.js
@@ -3,14 +3,28 @@ import {StyleSheet} from 'react-native'
 export default StyleSheet.create({
 
     container: {
-        flex: 1,
+        flex: 11,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    welcome: {
-        textAlign: 'center'
-    }
-    
+    containerWelcome:{
+        backgroundColor: 'blue',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        paddingBottom: 20,
+    },
+    button: {
+        backgroundColor: 'green',
+        color: 'blue',
+        marginTop: 20,
+        padding: 20,
+        borderRadius: 30,
+    },
+    buttonText: {
+        color: 'white'
+    },
+
 });
 
